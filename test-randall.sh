07afcfb845be34b9a56f0bd25be56b0dd003d770
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2021-05-20 01:31:36 -0700

    More tests

--- test-randall.sh
+++ test-randall.sh
@@ -1,3 +1,78 @@
 #!/usr/bin/bash
 
-a=$(./randall 5 | wc -c); if [ $a -eq 5 ]; then echo "success"; else echo "fail"; fi
+echo "Testing w/ no opt args..."
+for n in {0..100}
+do
+    x=$(./randall $n | wc -c)
+    if [ $x -ne $n ]
+    then
+       echo "Failed at NBYTES=$n"
+       exit 1
+    fi
+done
+echo "Done"
+
+input[0]="rdrand"
+input[1]="mrand48_r"
+input[2]="/dev/urandom"
+size=${#input[@]}
+
+echo "Testing w/ i args..."
+for n in {0..100}
+do
+    index=$(($RANDOM % $size))
+    i=${input[$index]}
+    x=$(./randall $n -i $i | wc -c)
+    if [ $x -ne $n ]
+    then
+       echo "Failed at NBYTES=$n INPUT=$i"
+       exit 1
+    fi
+done
+echo "Done"
+
+echo "Testing w/ o args"
+for n in {2..100}
+do
+    if [ $(($RANDOM % 2)) -eq 0 ]
+    then
+	o="stdio"
+    else
+	max=$(($n - 1))
+	o=$(($RANDOM % $max))
+	o=$(($o + 1))
+    fi
+    x=$(./randall $n -o $o | wc -c)
+    if [ $x -ne $n ]
+    then
+       echo "Failed at NBYTES=$n OUTPUT=$o"
+       exit 1
+    fi
+done
+echo "Done"
+
+echo "Testing w/ i and o args..."
+for n in {2..300}
+do
+    index=$(($RANDOM % $size))
+    i=${input[$index]}
+    if [ $(($RANDOM % 2)) -eq 0 ]
+    then
+	o="stdio"
+    else
+	max=$(($n - 1))
+	o=$(($RANDOM % $max))
+	o=$(($o + 1))
+    fi
+    x=$(./randall $n -i $i -o $o | wc -c)
+    if [ $x -ne $n ]
+    then
+       echo "Failed at NBYTES=$n INPUT=$i OUTPUT=$o"
+       exit 1
+    fi
+done
+echo "Done"
+
+echo "Testing finished"
+
+
